a230a99591b05e2689b3bfeb832a096e647c1abb
Utkarsh <vermau2k01@gmail.com>
2024-07-27 09:44:00 +0530

    Added dependencies of Thymeleaf and OpenApi

--- bsn/src/main/java/com/vermau2k01/bsn/security/SecurityConfig.java
+++ bsn/src/main/java/com/vermau2k01/bsn/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.vermau2k01.bsn.security;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
