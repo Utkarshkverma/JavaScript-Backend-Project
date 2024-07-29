3bfd533c3039ffbbc03e86a1d6546e56c2645945
Utkarsh <vermau2k01@gmail.com>
2024-07-29 23:34:50 +0530

    Project Manager

---
+++ project_manager/project_manager/src/test/java/com/vermau2k01/project_manager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vermau2k01.project_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
